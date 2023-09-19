060ced3750ae20f72e32ccf0b1b19b0aafe1ad8c
MaratheHarshad <harshadmarathe8@gmail.com>
2023-09-19 15:19:16 +0530

    string regex in shell_practice updated

--- cos/shell_practice/check_string_uppercase_lowercase.sh
+++ cos/shell_practice/check_string_uppercase_lowercase.sh
@@ -5,15 +5,22 @@ echo "Program to check entered string is upper case or lower case or mixed"
 read -p "Enter string : " string
 
 
-if [[ $string = [[:upper:]]+$ ]]
+# regular expression pattern should be check in double [] brackets
+# =~ is an operator in bash to compare a string with a regular expression pattern
+# ^ indicates start of the string
+# + indicates one or more characters (without + only string with 1 char is matched)
+# $ indicates end of the string
+
+if [[ $string =~ ^[[:upper:]]+$ ]]
 	then
 		echo "upper case string : $string"
 
 
+elif [[ $string =~ ^[[:lower:]]+$ ]]
+	then
+		echo "lower case string : $string"
+
+
+else
+	echo "string is mixed string : $string"
 fi
-
-#if [ $string == [[:lower:]*] ]
-#	then
-#		echo "lower case string : $string"
-
-#fi
